f823ddf38398fc4293999c8a55cead5c584a2c57
Freddy L <neo-ciber_94@hotmail.com>
2023-03-07 18:14:04 -0400

    Added bigint and symbol validator

---
+++ src/validators/common/bigint.validator.ts
@@ -0,0 +1,39 @@
+import { ValidationResult, Validator } from "../../core/validator";
+
+export interface BigIntValidatorOptions {
+  message?: string | ((value: unknown) => string);
+}
+
+export class BigIntValidator extends Validator<bigint> {
+  private readonly message: (value: unknown) => string;
+
+  constructor(options: BigIntValidatorOptions = {}) {
+    super();
+
+    const message = options.message;
+
+    if (message == null) {
+      this.message = numberErrorFactory;
+    } else if (typeof message === "string") {
+      this.message = () => message;
+    } else if (typeof message === "function") {
+      this.message = message;
+    } else {
+      throw new Error("invalid message type: " + typeof message);
+    }
+  }
+
+  parseSafe(value: unknown): ValidationResult<bigint> {
+    if (typeof value !== "bigint") {
+      return {
+        error: this.message(value),
+      };
+    }
+
+    return { success: true, value };
+  }
+}
+
+const numberErrorFactory = (value: unknown) => {
+  return `expected bigint but was ${typeof value}`;
+};
---
+++ src/validators/common/symbol.validator.ts
@@ -0,0 +1,39 @@
+import { ValidationResult, Validator } from "../../core/validator";
+
+export interface SymbolValidatorOptions {
+  message?: string | ((value: unknown) => string);
+}
+
+export class SymbolValidator extends Validator<symbol> {
+  private readonly message: (value: unknown) => string;
+
+  constructor(options: SymbolValidatorOptions = {}) {
+    super();
+
+    const message = options.message;
+
+    if (message == null) {
+      this.message = stringErrorFactory;
+    } else if (typeof message === "string") {
+      this.message = () => message;
+    } else if (typeof message === "function") {
+      this.message = message;
+    } else {
+      throw new Error("invalid message type: " + typeof message);
+    }
+  }
+
+  parseSafe(value: unknown): ValidationResult<symbol> {
+    if (typeof value !== "symbol") {
+      return {
+        error: this.message(value),
+      };
+    }
+
+    return { success: true, value };
+  }
+}
+
+const stringErrorFactory = (value: unknown) => {
+  return `expected symbol but was ${typeof value}`;
+};
